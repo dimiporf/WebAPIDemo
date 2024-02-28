d817559e3ec3a2e1ab0795ffd39969301d38123e
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-02-28 20:39:01 +0100

    Place routing info on top of controller class

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -3,36 +3,32 @@
 namespace WebAPIDemo.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtsController: ControllerBase
     {
-        [HttpGet]
-        [Route("/shirts")]
+        [HttpGet]        
         public string GetShirts()
         {
             return "Reading all the shirts";
         }
 
-        [HttpGet]
-        [Route("/shirts/{id}")]
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt with ID: {id}";
         }
 
         [HttpPost]
-        [Route("/shirts")]
         public string CreateShirt()
         {
             return $"Creating a shirt.";
         }
-        [HttpPut]
-        [Route("/shirts/{id}")]
+        [HttpPut("{id}")]        
         public string UpdateShirt(int id)
         {
             return $"Updating a shirt with ID: {id}";
         }
-        [HttpDelete]
-        [Route("/shirts/{id}")]
+        [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting a shirt with ID: {id}";
