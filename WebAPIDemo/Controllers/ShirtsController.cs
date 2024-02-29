72fb640195f6f0a264a3b19d1312e6ed5bc34cad
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-02-29 22:30:52 +0100

    Hardcoded Shirts Added

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -7,6 +7,15 @@ namespace WebAPIDemo.Controllers
     [Route("api/[controller]")]
     public class ShirtsController: ControllerBase
     {
+        private List<Shirt> shirts = new List<Shirt>() 
+        {
+            new Shirt { ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Men", Price = 30, Size = 10},
+            new Shirt { ShirtId = 2, Brand = "My Brand1", Color = "Black", Gender = "Men", Price = 35, Size = 12},
+            new Shirt { ShirtId = 3, Brand = "My Brand2", Color = "Pink", Gender = "Women", Price = 20, Size = 8},
+            new Shirt { ShirtId = 4, Brand = "My Brand3", Color = "Yellow", Gender = "Women", Price = 28, Size = 9}
+
+        };
+
         [HttpGet]        
         public string GetShirts()
         {
