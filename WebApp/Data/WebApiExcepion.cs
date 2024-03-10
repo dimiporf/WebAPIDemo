ed2087c2a8ff9fb58bc60106fb17a6e34b0aea34
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-03-10 19:09:01 +0100

    Error Handling in API Executer

---
+++ WebApp/Data/WebApiExcepion.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace WebApp.Data
+{
+    // Represents an exception that occurs during communication with a web API
+    public class WebApiException : Exception
+    {
+        // Gets or sets the error response received from the API
+        public ErrorResponse? ErrorResponse { get; set; }
+
+        // Constructor that initializes a new instance of the WebApiException class with the specified error JSON
+        public WebApiException(string errorJson)
+        {
+            // Deserialize the error JSON into an ErrorResponse object
+            ErrorResponse = JsonSerializer.Deserialize<ErrorResponse>(errorJson);
+        }
+    }
+
+}
--- WebApp/Data/WebApiExecuter.cs
+++ WebApp/Data/WebApiExecuter.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApp.Data
+﻿using System.Text.Json;
+
+namespace WebApp.Data
 {
     // Class responsible for executing HTTP requests to a web API
     public class WebApiExecuter : IWebApiExecuter
@@ -21,9 +23,19 @@
             // Creates a new HttpClient instance using the HttpClientFactory
             var httpClient = httpClientFactory.CreateClient(apiName);
 
-            // Sends an HTTP GET request to the specified relative URL and deserializes the response
-            return await httpClient.GetFromJsonAsync<T>(relativeUrl);
+            // Creates a new HttpRequestMessage for the GET request
+            var request = new HttpRequestMessage(HttpMethod.Get, relativeUrl);
+
+            // Sends the HTTP GET request and waits for the response
+            var response = await httpClient.SendAsync(request);
+
+            // Handle potential errors in the response
+            await HandlePotentialError(response);
+
+            // If the response is successful, reads the response content and deserializes it to the specified type
+            return await response.Content.ReadFromJsonAsync<T>();
         }
+
 
         // Invokes an HTTP POST request to the specified relative URL of the API with the provided object as content
         public async Task<T?> InvokePost<T>(string relativeUrl, T obj)
@@ -34,12 +46,13 @@
             // Sends an HTTP POST request to the specified relative URL with the provided object as JSON content
             var response = await httpClient.PostAsJsonAsync(relativeUrl, obj);
 
-            // Ensures that the HTTP response indicates success (status code 2xx)
-            response.EnsureSuccessStatusCode();
+            // Handle potential errors in the response
+            await HandlePotentialError(response);
 
-            // Reads the response content and deserializes it to the specified type
+            // If the response is successful, read the response content and deserialize it to the specified type
             return await response.Content.ReadFromJsonAsync<T>();
         }
+
 
         // Invokes an HTTP PUT request to the specified relative URL of the API with the provided object as content
         public async Task InvokePut<T>(string relativeUrl, T obj)
@@ -50,8 +63,8 @@
             // Sends an HTTP PUT request to the specified relative URL with the provided object as JSON content
             var response = await httpClient.PutAsJsonAsync(relativeUrl, obj);
 
-            // Ensures that the HTTP response indicates success (status code 2xx)
-            response.EnsureSuccessStatusCode();
+            // Handle potential errors in the response
+            await HandlePotentialError(response);
         }
 
         // Asynchronously sends an HTTP DELETE request to the specified relative URL of the API
@@ -63,8 +76,20 @@
             // Sends an HTTP DELETE request to the specified relative URL
             var response = await httpClient.DeleteAsync(relativeUrl);
 
-            // Ensures that the HTTP response indicates success (status code 2xx)
-            response.EnsureSuccessStatusCode();
+            // Handle potential errors in the response
+            await HandlePotentialError(response);
+        }
+
+        private async Task HandlePotentialError(HttpResponseMessage httpResponse)
+        {
+            if(!httpResponse.IsSuccessStatusCode)
+            {
+                // If the response indicates an error, read the error JSON from the response content
+                var errorJson = await httpResponse.Content.ReadAsStringAsync();
+
+                // Throw a WebApiException with the error JSON
+                throw new WebApiException(errorJson);
+            }
         }
 
     }
