10540eb99695ab8cb8f5163c379ff1a6181ad682
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-03-11 20:13:41 +0100

    Register Apps against Authority

---
+++ WebAPIDemo/Authority/AppRepository.cs
@@ -0,0 +1,35 @@
+﻿
+namespace WebAPIDemo.Authority
+{
+    // Static class responsible for managing applications and authentication
+    public static class AppRepository
+    {
+        // Static list to store application details
+        private static List<Application> _applications = new List<Application>()
+        {
+            // Initializing with a sample application
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "53D3C1E6-4587-4AD5-8C6E-A8E4BD59940E",
+                Secret = "0673FC70-0514-4011-B4A3-DF9BC03201BC"
+            }
+        };
+
+        // Method to authenticate an application based on provided clientId and secret
+        public static bool Authenticate(string clientId, string secret)
+        {
+            // Checking if any application matches the provided clientId and secret
+            return _applications.Any(x => x.ClientId == clientId && x.Secret == secret);
+        }
+
+        // Method to retrieve application details based on provided clientId
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            // Returning the first application that matches the provided clientId
+            return _applications.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
+    
